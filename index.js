334b43c7ce0792437d8c1d65af2a8354a1757fbe
Brandon Dega <brandondega@gmail.com>
2022-02-03 19:27:03 -0600

    notes added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ header.setAttribute('class','dog-name')
 header.append('Rizzo')
 content.append(header)
 
+//Cannot get image link to append
 let dogImage = document.createElement('img')
 dogImage.setAttribute('class','src')
 dogImage.append('./assets/rizzo.jpg')
@@ -23,4 +24,14 @@ let feedingTimes = document.createElement('h3')
 feedingTimes.append('Feeding Times:')
 content.append(feedingTimes)
 
+//Cannot get unordered list to populate
 let unorderedList = document.createElement('ul')
+let timeOne = document.createElement('li')
+timeOne.textContent = '9:00 am'
+unorderedList.append('timeOne')
+let timeTwo = document.createElement('li')
+timeTwo.textContent = '12:00 pm'
+unorderedList.append('timeTwo')
+let timeThree = document.createElement('li')
+timeThree.textContent = '5:00 pm'
+unorderedList.append('timeThree')
