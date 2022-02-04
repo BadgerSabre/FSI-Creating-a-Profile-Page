64c660fcb2726ff9be6f8b27fd83ea464e8968d4
Brandon Dega <brandondega@gmail.com>
2022-02-03 18:48:29 -0600

    adding list

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 let content = document.querySelector('.js-generated.content')
+
+let header = document.createElement('h1')
+header.setAttribute('class','dog-name')
+header.append('Rizzo')
+content.append(header)
+
+let dogImage = document.createElement('img')
+dogImage.setAttribute('class','src')
+dogImage.setAttribute('class','alt')
+dogImage.append('./assets/rizzo.jpg')
+dogImage.append('A picture of Rizzo')
+content.append(dogImage)
+
+let dogDetails = document.createElement('h3')
+dogDetails.setAttribute('class','dog-details')
+dogDetails.append('Description:')
+content.append(dogDetails)
+
+let paragraph = document.createElement('p')
+paragraph.append('This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.')
+content.append(paragraph)
+
+let feedingTimes = document.createElement('h3')
+feedingTimes.append('Feeding Times:')
+content.append(feedingTimes)
+
+let unorderedList = document.createElement('ul')
+unorderedList.setAttribute('class','unordered-list')
+
